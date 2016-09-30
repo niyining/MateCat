37766c8aba7651f623f1124ff30a885604cb75fd
domenico <domenico@translated.net>
2016-09-30 13:37:05 +0200

    Added Hack in lib/Utils/Engines/AbstractEngine.php
    to show the return value from APE because it is not yet implemented on MyMemory

--- lib/Utils/Engines/AbstractEngine.php
+++ lib/Utils/Engines/AbstractEngine.php
@@ -207,6 +207,9 @@ abstract class Engines_AbstractEngine {
 
         $rawValue = $this->_call( $url, $curl_opt );
 
+        //XXX HACK APE!
+        $rawValue = file_get_contents( "/var/www/matecat/mymemory_ape_response.json" );
+
         /*
          * $parameters['segment'] is used in MT engines,
          * they does not return original segment, only the translation.
