c50164b438369c566654a66cac84dfb6cea07a14
riccio82 <riccio82@gmail.com>
2016-10-03 13:13:37 +0200

    Removed hack for ape contribution

--- lib/Utils/Engines/AbstractEngine.php
+++ lib/Utils/Engines/AbstractEngine.php
@@ -206,10 +206,7 @@ abstract class Engines_AbstractEngine {
         }
 
         $rawValue = $this->_call( $url, $curl_opt );
-
-        //XXX HACK APE!
-//        $rawValue = file_get_contents( "/var/www/matecat/mymemory_ape_response.json" );
-
+        
         /*
          * $parameters['segment'] is used in MT engines,
          * they does not return original segment, only the translation.
